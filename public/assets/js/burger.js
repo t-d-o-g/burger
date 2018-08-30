c8106a92cde2aadb7c05552506fa54d493fd1974
Travis Collins <trc229@gmail.com>
2018-08-30 08:56:42 -0400

    Implement devour burger logic

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -15,4 +15,20 @@ $(() => {
       location.reload();
     });
   });
+
+  $('.devour-btn').on('click', function(event) {
+    const id = $(this).data('id');
+
+    const devouredTrue = {
+      devoured: 1
+    };
+
+    $.ajax('/api/burgers/' + id, {
+      type: 'PUT',
+      data: devouredTrue 
+    }).then(() => {
+      console.log(`Burger with id: ${id} devoured!`);
+      location.reload();
+    });
+  });
 });
